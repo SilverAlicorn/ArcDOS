eb7a857c856a8d14b467e5e0d82fa742ede8023e
Faye Duhamel <ikkakujyu@Faye-Duhamels-MacBook.local>
2011-09-03 15:31:50 -0700

    nonworking toolbox code

--- Misadventures Examples/sources/smartprint.s
+++ Misadventures Examples/sources/smartprint.s
@@ -57,10 +57,10 @@ init:
 nf: b nf
 	
 
-##########
-# Copy the 8-bit ASCII data in the text buffers to the video screen, as
-# 16-bit tile indices
-##########
+@------------------------------------------------------------------------------
+@ Copy the 8-bit ASCII data in the text buffers to the video screen, as
+@ 16-bit tile indices
+@------------------------------------------------------------------------------
 updateScreens:
 	mov r3,#0x300		@ Fill whole map
 	
@@ -72,13 +72,13 @@ updateScreens:
 	
 	b nf	
 
-##########
-# Takes a pointer to a string in r0 and prints it to the screen buffer
-# pointed in r1, at the cursor offset in r2. Attempting to write to an
-# area outside the buffer causes the uppermost line to be deleted,
-# shifts everything up one row, and resets the cursor to the end of
-# the last line.
-##########
+@------------------------------------------------------------------------------
+@ Takes a pointer to a string in r0 and prints it to the screen buffer
+@ pointed in r1, at the cursor offset in r2. Attempting to write to an
+@ area outside the buffer causes the uppermost line to be deleted,
+@ shifts everything up one row, and resets the cursor to the end of
+@ the last line.
+@------------------------------------------------------------------------------
 smartprint:
 	push {r4,r5,r6,lr}
 	mov r3,#0			@ String delimiter
@@ -154,7 +154,7 @@ testtext:
 	.asciz "test text"
 
 txtMonologue:
-	.asciz "We hold these truths to be self-evident, that all men are created equal, that they are endowed by their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness. — That to secure these rights, Governments are instituted among Men, deriving their just powers from the consent of the governed, — That whenever any Form of Government becomes destructive of these ends, it is the Right of the People to alter or to abolish it, and to institute new Government, laying its foundation on such principles and organizing its powers in such form, as to them shall seem most likely to effect their Safety and Happiness. Prudence, indeed, will dictate that Governments long established should not be changed for light and transient causes; and accordingly all experience hath shewn that mankind are more disposed to suffer, while evils are sufferable than to right themselves by abolishing the forms to which they are accustomed. But when a long train of abuses and usurpations, pursuing invariably the same Object evinces a design to reduce them under absolute Despotism, it is their right, it is their duty, to throw off such Government, and to provide new Guards for their future security. Such has been the patient sufferance of these Colonies; and such is now the necessity which constrains them to alter their former Systems of Government. The history of the present King of Great Britain is a history of repeated injuries and usurpations, all having in direct object the establishment of an absolute Tyranny over these States. To prove this, let Facts be submitted to a candid world."
+	.asciz "We hold these truths to be self-evident, that all men are created equal, that they are endowed by their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness. That to secure these rights, Governments are instituted among Men, deriving their just powers from the consent of the governed, That whenever any Form of Government becomes destructive of these ends, it is the Right of the People to alter or to abolish it, and to institute new Government, laying its foundation on such principles and organizing its powers in such form, as to them shall seem most likely to effect their Safety and Happiness. Prudence, indeed, will dictate that Governments long established should not be changed for light and transient causes; and accordingly all experience hath shewn that mankind are more disposed to suffer, while evils are sufferable than to right themselves by abolishing the forms to which they are accustomed. But when a long train of abuses and usurpations, pursuing invariably the same Object evinces a design to reduce them under absolute Despotism, it is their right, it is their duty, to throw off such Government, and to provide new Guards for their future security. Such has been the patient sufferance of these Colonies; and such is now the necessity which constrains them to alter their former Systems of Government. The history of the present King of Great Britain is a history of repeated injuries and usurpations, all having in direct object the establishment of an absolute Tyranny over these States. To prove this, let Facts be submitted to a candid world."
 buffer_screen1:
 	.space 768
 
