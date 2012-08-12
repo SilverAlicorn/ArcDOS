119626d62c0eceeb5441e74893413f938824a2f9
SilverAlicorn <ikkakujyu@gmail.com>
2012-08-12 01:07:38 -0700

    Adding new DLDI stuff

--- filetest/build.sh
+++ filetest/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-arm-none-eabi-gcc -specs=arcdos_arm9.specs -o arm9.elf main.c console.c consolefont.c asmfuncs.s
+arm-none-eabi-gcc -Wall -specs=arcdos_arm9.specs -o arm9.elf main.c console.c consolefont.c asmfuncs.s io_dldi.s
 
 arm-none-eabi-objcopy -O binary arm9.elf arm9.bin
 
--- filetest/main.c
+++ filetest/main.c
@@ -1,12 +1,31 @@
 #include "ndsarm9.h"
 #include "console.h"
 
+struct io_interface {
+	unsigned long ul_iotype;
+	unsigned long ul_features;
+	int (*fn_startup)(void);
+	int (*fn_ininserted)(void);
+	int (*fn_readsectors)(int sector, int numsecs, void* buffer);
+	int (*fn_writesectors)(int sector, int numsecs, void* buffer);
+	int (*fn_clearstatus)(void);
+	int (*fn_shutdown)(void);
+};
+
+extern struct io_interface _io_dldi;
+
 int main()
 {
 	console_init();
 
 //	puts("Hello, world\n", MAIN_SCREEN);
-	putline("weedlord bonerhitler", 16);
+	putline("Hello ArcDOS!", 0);
+
+	_io_dldi.fn_startup();
+
+	_io_dldi.fn_readsectors(0, 1, 0x2100000);
+
+	putline("It sorta worked!", 3);
 
 	while (1);
 }
